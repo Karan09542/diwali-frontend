19519f34a27544dff962d5a0019b93073897c9d0
Karan09542 <karan09542@gmail.com>
2025-11-14 01:58:04 +0530

    add vercel.json

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
     proxy: {
-      "/api": "http://localhost:5001"
+      "/api": import.meta.env.VITE_API_URL || "http://localhost:5001"
     },
     hmr: true,
   },
